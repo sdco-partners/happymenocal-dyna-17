cd13a273a6d5952d1d7e565b5a038286b603cf5c
Asif Ahmed <asif@stitchdesignco.com>
2017-10-02 10:33:46 -0400

    remove height element from image attributes

---
+++ content/themes/happy/components/head/head.php
@@ -0,0 +1,39 @@
+<?php 
+  /*
+  * Section =>  HEAD
+  */
+
+  if(have_rows('navigation', 'option')) : 
+  while(have_rows('navigation', 'options')) : 
+    the_row(); ?>
+    <div class="left-nav">
+      <h2 id="toggle">Menu</h2>
+    </div>
+    <div class="drop-down">
+    <?php if(have_rows('left_nav')) : 
+      while(have_rows('left_nav')) : 
+        the_row();
+        $title = get_sub_field('title');
+        $link = get_sub_field('link');
+        $class = strtolower(str_replace(' ', '-', $title));
+         ?>
+        <a id="<?php echo $class; ?>" href="<?php echo $link; ?>">
+          <span><?php echo $title; ?></span>
+        </a>
+      <?php endwhile;
+      endif; ?>
+  </div>
+  <div class="transparency"></div>
+  <div class="logo">
+    <img class="main-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-main.gif">
+  </div>
+  <div class="right-link">
+    <?php if(have_rows('right_nav')) : 
+      while(have_rows('right_nav')) :
+        the_row(); ?>
+        <a href="<?php echo get_sub_field('link'); ?>"><?php echo get_sub_field('title'); ?></a>
+      <?php endwhile;
+    endif; ?>
+  </div>
+  <?php endwhile;
+endif; ?>
--- content/themes/happy/header.php
+++ content/themes/happy/header.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * Header
  *
@@ -18,10 +18,7 @@
   <script type="text/javascript">
 	document.documentElement.setAttribute("data-browser", navigator.userAgent);
   </script>
-
   <script src="//localhost:35729/livereload.js"></script>
-
-
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -38,37 +35,5 @@
 
 <!-- SECTION: HEADER -->
 <header>
-<?php if(have_rows('navigation', 'option')) : 
-  while(have_rows('navigation', 'options')) : 
-    the_row(); ?>
-    <div class="left-nav">
-      <h2 id="toggle">Menu</h2>
-    </div>
-    <div class="drop-down">
-    <?php if(have_rows('left_nav')) : 
-      while(have_rows('left_nav')) : 
-        the_row();
-        $title = get_sub_field('title');
-        $link = get_sub_field('link');
-        $class = strtolower(str_replace(' ', '-', $title));
-         ?>
-        <a id="<?php echo $class; ?>" href="<?php echo $link; ?>">
-          <span><?php echo $title; ?></span>
-        </a>
-      <?php endwhile;
-      endif; ?>
-  </div>
-  <div class="logo">
-    <img class="main-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-main.gif">
-  </div>
-  <div class="right-link">
-    <?php if(have_rows('right_nav')) : 
-      while(have_rows('right_nav')) :
-        the_row(); ?>
-        <a href="<?php echo get_sub_field('link'); ?>"><?php echo get_sub_field('title'); ?></a>
-      <?php endwhile;
-    endif; ?>
-  </div>
-  <?php endwhile;
-endif; ?>
+  <?php include(locate_template('components/head/head.php')); ?>
 </header>
